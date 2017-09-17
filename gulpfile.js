900881632541b2f472c0d112a84a60061ee7d002
wang7211401 <wang7211401@163.com>
2017-09-17 20:06:00 +0800

    转成ES5

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp');
+var del = require('del');
+var postcss = require('gulp-postcss');
+const babel = require('gulp-babel');
+var minify = require('gulp-minify');
+const eslint = require('gulp-eslint');
+
+
+
+gulp.task('rm', function() {
+    return del(['dist/**']);
+});
+
+gulp.task('css', ['rm'], function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        // dest: destination 目的地
+        // dist: distribution 发布
+        .pipe(gulp.dest('./dist'));
+});
+gulp.task('css-watch', function () {
+    var postcss      = require('gulp-postcss');
+    var sourcemaps   = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./src/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        // dest: destination 目的地
+        // dist: distribution 发布
+        .pipe(gulp.dest('./dist'));
+});
+gulp.task('lint',function(){
+    return gulp.src('./src/*.js')
+            .pipe(eslint())
+            .pipe(eslint.format())
+            .pipe(eslint.failAfterError());
+});
+gulp.task('js', ['rm'],function(){
+        gulp.src('src/*.js')
+            .pipe(babel({
+                presets: ['env']
+            }))
+            .pipe(minify({noSource: true,ext:{ min:'.js'}}))
+            .pipe(gulp.dest('dist'))
+});
+gulp.task('js-watch', function(){
+        gulp.src('src/*.js')
+            .pipe(babel({
+                presets: ['env']
+            }))
+            .pipe(minify({noSource: true,ext:{ min:'.js'}}))
+            .pipe(gulp.dest('dist'))
+
+});
+
+gulp.task('default', ['rm', 'css', 'lint','js']);
+
+gulp.task('watch', function(){
+    var watcher1 = gulp.watch('src/**/*.js', ['js-watch']);
+    watcher1.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+    var watcher2 = gulp.watch('src/**/*.css', ['css-watch']);
+    watcher2.on('change', function(event) {
+        console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
---
+++ src/resume.js
@@ -0,0 +1,101 @@
+$('.tabs').on('click', 'li', function (e) {
+    $('.container-content').addClass('bounceIn');
+    setTimeout(function () {
+        $('.container-content').removeClass('bounceIn');
+    }, 1000);
+    let $li = $(e.currentTarget);
+    let index = $li.index();
+    $li.addClass('active').siblings('.active').removeClass('active');
+    $('.tab-content > section').eq(index).addClass('active').siblings('.active').removeClass('active')
+});
+setInterval(function () {
+    var index = $('.cycle-slide.active').index();
+    index = index + 1;
+    if (index === 3) {
+        index = 0
+    }
+    $('.cycle-slide').eq(index).addClass('active').siblings().removeClass('active')
+}, 3000);
+var list = true;
+var list2 = true;
+var list3 = true;
+var list4 = true;
+var list5 = true;
+if($('.tab-content>section').hasClass('active')){
+    list = false
+}
+$('.profile').prev('.tab-content-title').click(function(){
+    if($('.profile').hasClass('active')){
+        list = false
+    }else if($('.profile').siblings('section').hasClass('active') || !$('.profile').hasClass('active')){
+        list = true
+    }
+    if(list){
+        $('.profile').addClass('active').siblings('section.active').removeClass('active');
+        list = false
+    }else{
+        $('.profile').removeClass('active');
+        list = true
+    }
+    $('html, body').animate({scrollTop: $('.tab-content-title').offset().top - 50}, 600);
+});
+$('.projects').prev('.tab-content-title').click(function(){
+    if($('.projects').hasClass('active')){
+        list2 = false
+    }else if($('.projects').siblings('section').hasClass('active') || !$('.projects').hasClass('active')){
+        list2 = true
+    }
+    if(list2){
+        $('.projects').addClass('active').siblings('section.active').removeClass('active');
+        list2 = false
+    }else{
+        $('.projects').removeClass('active');
+        list2 = true
+    }
+    $('html, body').animate({scrollTop: $('.tab-content-title').offset().top - 50}, 600);
+});
+$('.demos').prev('.tab-content-title').click(function(){
+    if($('.demos').hasClass('active')){
+        list3 = false
+    }else if($('.demos').siblings('section').hasClass('active') || !$('.demos').hasClass('active')){
+        list3 = true
+    }
+    if(list3){
+        $('.demos').addClass('active').siblings('section.active').removeClass('active');
+        list3 = false
+    }else{
+        $('.demos').removeClass('active');
+        list3 = true
+    }
+    $('html, body').animate({scrollTop: $('.tab-content-title').offset().top - 50}, 600);
+});
+$('.skills').prev('.tab-content-title').click(function(){
+    if($('.skills').hasClass('active')){
+        list4 = false
+    }else if($('.skills').siblings('section').hasClass('active') || !$('.skills').hasClass('active')){
+        list4 = true
+    }
+    if(list4){
+        $('.skills').addClass('active').siblings('section.active').removeClass('active');
+        list4 = false
+    }else{
+        $('.skills').removeClass('active');
+        list4 = true
+    }
+    $('html, body').animate({scrollTop: $('.tab-content-title').offset().top - 50}, 600);
+});
+$('.address').prev('.tab-content-title').click(function(){
+    if($('.address').hasClass('active')){
+        list5 = false
+    }else if($('.address').siblings('section').hasClass('active') || !$('.address').hasClass('active')){
+        list5 = true
+    }
+    if(list5){
+        $('.address').addClass('active').siblings('section.active').removeClass('active');
+        list5 = false
+    }else{
+        $('.address').removeClass('active');
+        list5 = true
+    }
+    $('html, body').animate({scrollTop: $('.tab-content-title').offset().top - 50}, 600);
+});
